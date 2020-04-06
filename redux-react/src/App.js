041652bd0cb63b8ca4d24f038e8245688c4ccafe
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-06 18:33:47 +0200

    Add Hooks ( useSelector, useDispatch)

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import HooksCakeContainer from './components/HooksCakeContainer';
 
 
 function App() {
   return (
     <Provider store= { store }>
+      <HooksCakeContainer />
       <CakeContainer />
     </Provider>
   );
