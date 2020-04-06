20c5e19b41647b38d107bf3fe60e750d52916c68
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-06 14:46:26 +0200

    Connent and dispatch to store

--- redux-react/src/components/CakeContainer.js
+++ redux-react/src/components/CakeContainer.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import  buyCake from '../redux/cake/cakeActions';
 
-const CakeContainer = () => {
+function CakeContainer(props) {
   return (
     <div>
-      <h1>Number of cakes </h1>
-      <button>Buy cake</button>
+      <h1>Number of cakes - { props.numOfCakes } </h1>
+      <button onClick= { props.buyCake }>Buy cake</button>
     </div>)
 }
-export default CakeContainer;
+
+// ##How we can get the state from store, and dispatch an action from the component ?
+// first step we define a new function "mapStateToProps" 
+
+//Our props in this component will receive an additional props calls numOfCakes which
+// reflects to the numOfCakes in the redux store
+
+const mapStateToProps = state => {
+  return {
+    numOfCakes: state.numOfCakes
+  }
+}
+
+//The step number two
+// difine a nother function "mapDispatchToProps"
+
+//Our props in this component will receive an additional props calls buyCake which
+// will dispatch to the cake to store
+
+const mapDispatchToProps = dispatch => {
+  return {
+    buyCake: () => {
+      dispatch(buyCake())
+    }
+  }
+
+  //The step number three
+  // connect these 2 functions with our react component 
+
+}
+export default connect(mapStateToProps, mapDispatchToProps) (CakeContainer);
