d8c70537213d89b62fa992ad6629740ed7dc8497
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-09 00:22:52 +0200

    Replace spaces4 with spaces 2

--- redux-react/src/components/CakeContainer.js
+++ redux-react/src/components/CakeContainer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import  buyCake from '../store/actions/cakeActions';
+import buyCake from '../store/actions/cakeActions';
 
 function CakeContainer() {
-    const numOfCakes = useSelector(state => state.cake.numOfCakes)
-    const dispatch = useDispatch();
-    return (
-        <div>
-            <h2>Num of Cakes from hook - { numOfCakes }</h2>
-            <button onClick= { () => dispatch(buyCake())}>Buy Cake </button>
-        </div>
-    )
+  const numOfCakes = useSelector(state => state.cake.numOfCakes)
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Num of Cakes from hook - {numOfCakes}</h2>
+      <button onClick={() => dispatch(buyCake())}>Buy Cake </button>
+    </div>
+  )
 }
 
 export default CakeContainer;
--- redux-react/src/components/IceCreamContainer.js
+++ redux-react/src/components/IceCreamContainer.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import  buyIceCream from '../store/actions/iceCreamActions';
+import buyIceCream from '../store/actions/iceCreamActions';
 
 function IceCreamContainer() {
-    const numOfIceCreams = useSelector(state => state.iceCream.numOfIceCreams)
-    const dispatch = useDispatch();
-    return (
-        <div>
-            <h2>Num of Icecream - { numOfIceCreams }</h2>
-            <button onClick= { () => dispatch(buyIceCream())}>Buy Icecream Now</button>
-        </div>
-    )
+  const numOfIceCreams = useSelector(state => state.iceCream.numOfIceCreams)
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <h2>Num of Icecream - {numOfIceCreams}</h2>
+      <button onClick={() => dispatch(buyIceCream())}>Buy Icecream Now</button>
+    </div>
+  )
 }
 
 export default IceCreamContainer;
