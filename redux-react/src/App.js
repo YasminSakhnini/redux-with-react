41bc7b9ff8e34087177acb1b7b44f17523e67a02
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-09 18:26:00 +0200

    Add list of users from API using async action

--- redux-react/src/App.js
+++ redux-react/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import CakeContainer from './components/CakeContainer';
+//import CakeContainer from './components/CakeContainer';
 import { Provider } from 'react-redux';
 import store from './store/store';
-import IceCreamContainer from './components/IceCreamContainer';
+//import IceCreamContainer from './components/IceCreamContainer';
+import UserContainer from './components/UserContainer';
 
 
 function App() {
   return (
-    <Provider store= { store }>
-      <CakeContainer />
-      <IceCreamContainer />
+    <Provider store={store}>
+      {/*<CakeContainer />
+      <IceCreamContainer /> */}
+      <UserContainer />
     </Provider>
   );
 }
--- redux-react/src/store/actions/userActions.js
+++ redux-react/src/store/actions/userActions.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
   FETCH_USERS_REQUEST,
   FETCH_USERS_SUCCESS,
@@ -18,12 +19,30 @@ export const fetchUsersSuccess = users => {
   }
 }
 
-export const fetchUsersReq = error => {
+export const fetchUsersFailuer = error => {
   return {
     type: FETCH_USERS_FAILURE,
     payload: error
   }
 }
+
+const fetchUsers = () => {
+  return (dispatch) => {
+    dispatch(fetchUsersReq())
+    axios.get('https://jsonplaceholder.typicode.com/users')
+      .then(response => {
+        const users = response.data
+        dispatch(fetchUsersSuccess(users))
+      })
+      .catch(error => {
+        const errorMsg = error.message
+        dispatch(fetchUsersFailuer(errorMsg))
+      })
+
+  }
+}
+
+export default fetchUsers;
 
 
 
