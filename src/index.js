cc340bc2a47a1c71db506d8e30060ad20d23ef0c
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-24 18:34:50 +0200

    Redux Architecture

--- src/index.js
+++ src/index.js
@@ -12,3 +12,9 @@ let updated = publish(book);
 console.log('book', book);
 
 console.log('updated', updated);
+
+// Steps to build a redux app
+//Design the store
+//Define the actions
+//Create reducer
+//Set up the store
