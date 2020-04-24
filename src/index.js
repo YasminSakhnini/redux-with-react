071fef820c58b70be7308d5bae0890925d1e3db6
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-24 18:00:36 +0200

    Immer

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
-import { Map } from 'immutable';
+import { produce } from 'immer';
 
-//let book = { title: "Boys shouls be boys" };
-let book = Map({ title: "Boys shouls be boys" });
-
-//This is a problem with the immutable library
-// because we can not git the title by book.title
-// but we use book.get("title")
-//and to get back a plain js object we need to use toJS()
-console.log('book', book.toJS());
-
+let book = { title: "Boys shouls be boys" };
 
 function publish(book) {
-  return book.set("isPublished", true);
+  return produce(book, draftBook => {
+    draftBook.isPublished = true;
+  });
 }
 
-book = publish(book);
-console.log('book', book.toJS());
+let updated = publish(book);
+console.log('book', book);
+
+console.log('updated', updated);
