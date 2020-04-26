2d4730880899c05001523608f0d7e7b711a0a853
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-26 23:16:30 +0200

    Subscribing to the store

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import store from './store';
 
+const unsubscribe = store.subscribe(() => {
+  console.log('Store changed!', store.getState());
+})
 store.dispatch({
   type: 'bugAdded',
   payload: {
     description: 'bug1'
   }
 });
+unsubscribe();
 
 store.dispatch({
   type: 'bugRemoved',
