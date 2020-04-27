fc676d3cb4013add1a2bb8598e2c2f0e03e39657
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-27 23:18:34 +0200

    Creating slice with toolkit

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,32 +1,26 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createAction, createReducer, createSlice } from '@reduxjs/toolkit';
 
-const bugUpdated = createAction("bugUpdated");
-console.log('bugUpdated', bugUpdated({id: 1}));
-
-//Action cretors
-export const bugAdded = createAction("bugAdded");
-export const bugResolved = createAction("bugResolved");
-export const bugRemoved = createAction("bugRemoved");
-
-//Reducer
 let lastId = 0;
-createReducer([], {
-   //key: value
-   //actions: functions (event => event handler)
-  //with [ ] we will dynamically computed the name 
-  //of the prosperity  "bugAdded", "bugResolved"
-  //so if I want to change "bugAdded" on line 7 I dont need to touch this function
-  [bugAdded.type]: (bugs, action) => {
-    bugs.push({
+
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    // actions => action handlers
+    bugAdded: (bugs, action) => {
+      bugs.push({
       id: ++lastId,
       description: action.payload.description,
       resolved: false
     })
-  },
-
-  [bugResolved.type]: (bugs, action) => {
-    const index = bugs.findIndex(bug => bug.id === action.payload.id)
-    bugs[index].resolved = true;
-  },
-
+    },
+    bugResolved: (bugs, action) => {
+      const index = bugs.findIndex(bug => bug.id === action.payload.id)
+      bugs[index].resolved = true;
+    }
+    
+  }
 });
+
+export const { bugAdded, bugResolved } = slice.actions;
+export default slice.reducer;
