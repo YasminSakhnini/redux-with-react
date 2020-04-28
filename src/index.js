7be68ffb4942f3f0110a1d2cb57c0dca7ec4a3f4
Yasmin Sakhnini <yasmin.rebhi.sakhnini@gmail.com>
2020-04-28 11:12:31 +0200

    Add getUnresolvedBugs selctor

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import configureStore from './store/configureStore';
-import * as actions from './store/bugs';
+import { bugAdded, bugResolved, getUnresolvedBugs } from './store/bugs';
 import { projectAdded } from './store/projects';
 
 const store = configureStore();
@@ -7,11 +7,24 @@ const store = configureStore();
 store.subscribe(() => {
   console.log('Store changed!');
 })
-store.dispatch(actions.bugAdded({ description: 'Bug 1'}));
-store.dispatch(actions.bugAdded({ description: 'Bug 2'}));
-store.dispatch(actions.bugAdded({ description: 'Bug 3'}));
-store.dispatch(actions.bugResolved({id: 1}));
+store.dispatch(projectAdded({name: 'Project 1'}))
+store.dispatch(bugAdded({ description: 'Bug 1'}));
+store.dispatch(bugAdded({ description: 'Bug 2'}));
+store.dispatch(bugAdded({ description: 'Bug 3'}));
+store.dispatch(bugResolved({id: 1}));
 
-store.dispatch(projectAdded({name: 'project 1'}));
+const unReslovedBugs = getUnresolvedBugs(store.getState())
+console.log('unReslovedBugs', unReslovedBugs);
+
+
+
+
+//Better way than this to select part of the state is to create a selctor  "getUnresolvedBugs"
+/*
+const unReslovedBugs = store
+  .getState()
+  .entities.bugs.filter(bug => !bug.resolved )
+console.log('unReslovedBugs', unReslovedBugs);
 
 console.log('store', store.getState());
+*/
